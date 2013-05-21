017541f9ae8fd560a596de36adf7bc2f9e2ec21d
Aaron Seibert <aaron@binaryitsystems.com>
2013-05-21 08:24:27 -0400

    First commit

---
+++ CloudManager/build/source/buildConfig/debug/com/binaryitsystems/cloudmanager/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.binaryitsystems.cloudmanager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ CloudManager/src/main/java/com/binaryitsystems/cloudmanager/rackspace/RackspaceMain.java
@@ -0,0 +1,49 @@
+package com.binaryitsystems.cloudmanager.rackspace;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by aaron on 5/16/13.
+ */
+public class RackspaceMain {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<ProviderService> ITEMS = new ArrayList<ProviderService>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, ProviderService> ITEM_MAP = new HashMap<String, ProviderService>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new ProviderService("1", "Test"));
+        addItem(new ProviderService("2", "Service"));
+        addItem(new ProviderService("3", "Now"));
+    }
+
+    private static void addItem(ProviderService item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    public static class ProviderService {
+        public String id;
+        public String content;
+
+        public ProviderService(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
